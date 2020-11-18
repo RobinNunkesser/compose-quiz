8c47d421b8d330d316be0c3a27d460bda997bd50
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-11-18 11:30:38 +0100

    Quiz on Wear OS

--- WearQuiz/app/src/main/java/de/hshl/isd/wearquiz/MainFragment.kt
+++ WearQuiz/app/src/main/java/de/hshl/isd/wearquiz/MainFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainFragment : Fragment() {
@@ -24,8 +28,37 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        val questionTextView = requireActivity().findViewById<TextView>(R.id.questionTextView)
+        val falseButton = requireActivity().findViewById<ImageButton>(R.id.falseButton)
+        val trueButton = requireActivity().findViewById<ImageButton>(R.id.trueButton)
+        val skipButton = requireActivity().findViewById<ImageButton>(R.id.skipButton)
+
+        trueButton.setOnClickListener {
+            viewModel.evaluateAnswer(true)
+            viewModel.increaseIndex()
+        }
+
+        falseButton.setOnClickListener {
+            viewModel.evaluateAnswer(false)
+            viewModel.increaseIndex()
+        }
+
+        skipButton.setOnClickListener {
+            viewModel.increaseIndex()
+            viewModel.skippedQuestions++
+        }
+
+        viewModel.question.observe(viewLifecycleOwner, Observer { questionTextView.text = it })
+        viewModel.answer.observe(viewLifecycleOwner, Observer {
+            it?.let { answer ->
+                Toast.makeText(
+                    requireContext(),
+                    answer, Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 
 }
--- WearQuiz/app/src/main/java/de/hshl/isd/wearquiz/MainViewModel.kt
+++ WearQuiz/app/src/main/java/de/hshl/isd/wearquiz/MainViewModel.kt
@@ -1,7 +1,54 @@
 package de.hshl.isd.wearquiz
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val questions = listOf(
+        Pair("Das Videospiel Donkey Kong sollte ursprünglich Popeye als Hauptfigur haben.", true),
+        Pair("Die Farbe Orange wurde nach der Frucht benannt.", true),
+        Pair("In der griechischen Mythologie ist Hera die Göttin der Ernte.", false),
+        Pair("Liechtenstein hat keinen eigenen Flughafen.", true),
+        Pair("Die meisten Subarus werden in China hergestellt.", false)
+    )
+
+    var correctAnswers = 0
+    var wrongAnswers = 0
+    var skippedQuestions = 0
+    val answeredQuestions: Int
+        get() = correctAnswers + wrongAnswers + skippedQuestions
+
+    private val _index = MutableLiveData<Int>()
+    val index: LiveData<Int>
+        get() = _index
+
+    init {
+        _index.value = 0
+    }
+
+    val question: LiveData<String> =
+        Transformations.map(index) { index -> questions[index].first }
+
+    private val _answer = MutableLiveData<Int?>()
+    val answer: LiveData<Int?>
+        get() = _answer
+
+    fun increaseIndex() {
+        _index.value = (_index.value!! + 1) % questions.count()
+        _answer.value = null
+    }
+
+    fun evaluateAnswer(answer: Boolean) {
+        if (answer == questions[_index.value!!].second) {
+            _answer.value = R.string.label_correct
+            correctAnswers++
+        } else {
+            _answer.value = R.string.label_wrong
+            wrongAnswers++
+        }
+    }
+
 }
