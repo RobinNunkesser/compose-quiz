f19e6e635b9189c236c8b80d9a0c3217a77288a7
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-08-19 10:26:39 +0200

    Jetpack Compose 1.0.0

--- QuizCompose/app/src/main/java/de/hshl/isd/quizcompose/QuizScreen.kt
+++ QuizCompose/app/src/main/java/de/hshl/isd/quizcompose/QuizScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 
 @ExperimentalAnimationApi
